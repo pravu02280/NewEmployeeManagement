ab9f0791b32414f2488b165ef1788d17ac3ea278
AjayKarki <ajaykarki333@gmail.com>
2018-05-20 13:48:04 +0545

    Test removed

--- MyLedger/urls.py
+++ MyLedger/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from testapp.views import testview
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', testview),
 ]
--- testapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import json
-# Create your views here.
-
-def testview(request):
-    return HttpResponse(json.dumps({'name': 'Ajay', 'last name': 'Karki'}), content_type='application/json')
