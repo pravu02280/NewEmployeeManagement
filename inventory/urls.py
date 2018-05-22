5db5d67bb75950738fb03ebe9d94515cc5d26bd9
AjayKarki <ajaykarki333@gmail.com>
2018-05-22 19:23:39 +0545

    Add to invenntory removed

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 app_name = 'inventory'
 urlpatterns = [
-    path('addtoinventory/', views.AddToInventoryView.as_view(), name='addtoinventory'),
     path('viewinventory/',views.viewInventoryView.as_view(), name='viewinventory'),
 ]
