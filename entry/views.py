75a6cd19c0bca53012b646955dfe4aa1950967f2
AjayKarki <ajaykarki333@gmail.com>
2018-06-03 20:23:21 +0545

    PurchaseShow View Completed

--- entry/views.py
+++ entry/views.py
@@ -59,7 +59,7 @@ class PurchaseDetailView(generic.detail.DetailView):
         return HttpResponseRedirect(reverse('home'))
 
 class PurchaseListView(generic.list.ListView):
-    model =PurchaseDetail
+    model =Purchase
 
     def get_context_data(self,**kwargs):
         context = super().get_context_data(**kwargs)
